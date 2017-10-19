b72ed9ac1f23f3ade090ae2ca2bb81adb17ecc85
Jason K. MacDuffie <taknamay@gmail.com>
2017-10-18 22:32:34 -0400

    implement attributes

--- web.body.scm
+++ web.body.scm
@@ -23,8 +23,29 @@
   (cond
    ((list? s)
     (let ((tag (escape-html-string (symbol->string (car s)))))
-      (string-append
-       "<" tag ">" (apply string-append (map sxml->xml (cdr s))) "</" tag ">")))
+       ;; Check if there are any attributes
+       (let ((has-attribute? (and (pair? (cdr s))
+                                  (pair? (cadr s))
+                                  (eq? '|@| (car (cadr s))))))
+         (string-append
+          "<" tag
+          (if has-attribute?
+              (apply string-append
+                     (map (lambda (p)
+                            (unless (symbol? (car p))
+                              (error "symbol expected, got" (car p)))
+                            (string-append " "
+                                           (symbol->string (car p))
+                                           "=\""
+                                           (escape-html-string (cadr p))
+                                           "\""))
+                          (cdr (cadr s))))
+              "")
+          ">"
+          (apply string-append
+                 (map sxml->xml
+                      (if has-attribute? (cddr s) (cdr s))))
+          "</" tag ">"))))
    ((string? s)
     (escape-html-string s))
    ((symbol? s)
