8f0783faacb0a2f8cf460bfa706510e2a3f0846d
Jason K. MacDuffie <taknamay@gmail.com>
2017-10-31 21:16:41 -0400

    allow reading cgi vars

--- web.body.scm
+++ web.body.scm
@@ -73,3 +73,60 @@
 
 (define (load-static path)
   (display (read-entire-file path)))
+
+
+;; Environment variables
+(define-syntax define-env
+  (syntax-rules ()
+    ((_ proc-name env-name)
+     (define (proc-name) (get-environment-variable env-name)))))
+
+(define-syntax define-each-env
+  (syntax-rules ()
+    ((_ (proc-name env-name))
+     (define-env proc-name env-name))
+    ((_ (proc-name env-name) rest ...)
+     (begin
+       (define-env proc-name env-name)
+       (define-each-env rest ...)))))
+
+(define-each-env
+  (document-root "DOCUMENT_ROOT")
+  (http-cookie "HTTP_COOKIE")
+  (http-host "HTTP_HOST")
+  (http-referer "HTTP_REFERER")
+  (http-user-agent "HTTP_USER_AGENT")
+  (https "HTTPS")
+  (path "PATH")
+  (query-string "QUERY_STRING")
+  (remote-addr "REMOTE_ADDR")
+  (remote-host "REMOTE_HOST")
+  (remote-port "REMOTE_PORT")
+  (remote-user "REMOTE_USER")
+  (request-method "REQUEST_METHOD")
+  (request-uri "REQUEST_URI")
+  (script-filename "SCRIPT_FILENAME")
+  (script-name "SCRIPT_NAME")
+  (server-port "SERVER_PORT")
+  (server-software "SERVER_SOFTWARE"))
+
+
+#|
+          document-root http-cookie http-host http-referer
+          http-user-agent https path query-string
+          remote-addr remote-host remote-port remote-user
+          request-method request-uri script-filename
+          script-name server-port server-software))
+
+
+
+(define (query-string)
+  (get-environment-variable "QUERY_STRING"))
+
+(define (content-length)
+  (get-environment-variable "CONTENT_LENGTH"))
+
+(define (path-info)
+  (get-environment-variable "PATH_INFO"))
+
+|#
--- web.sld
+++ web.sld
@@ -3,6 +3,13 @@
   (import (scheme base)
           (scheme write)
           (scheme file)
-          (scheme load))
-  (export print mime-html declare-html (rename display print-raw) load load-static)
+          (scheme load)
+          (srfi 98))
+  (export print mime-html declare-html
+          (rename display print-raw) load load-static
+          document-root http-cookie http-host http-referer
+          http-user-agent https path query-string
+          remote-addr remote-host remote-port remote-user
+          request-method request-uri script-filename
+          script-name server-port server-software)
   (include "./web.body.scm"))
